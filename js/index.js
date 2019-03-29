1b87cd9815b1d6d9cfb1d91aa3823e767b554f74
Leslie Thompson <lcrthompson@gmail.com>
2019-03-28 17:25:49 -0700

    redone welcome page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// DROP DOWN SIGN IN
+
 //const dropdown = document.querySelector(".dropdown");
 const signcontain = document.querySelector(".signcontain");
 //const toggle =
@@ -8,14 +10,42 @@ signIn.addEventListener("click", () => {
   signcontain.classList.toggle("hidden");
 });
 
-// signIn.addEventListener("click", () => {
-//   if ((dropdown.style.display = "none")) {
-//     dropdown.style.display = "block";
-//     console.log(dropdown);
-//   } else {
-//     dropdown.style.display = "none";
-//     console.log(dropdown);
-//   }
+// CAROUSEL
+class Carousel {
+  constructor(images) {
+    this.images = images;
+    this.imgs = images.querySelectorAll("img");
+    this.leftBtn = images.querySelector(".leftbtn");
+    this.rightBtn = images.querySelector(".rightbtn");
+    this.rightBtn.addEventListener("click", () => {
+      this.rightClick();
+    });
+    this.leftBtn.addEventListener("click", () => {
+      this.leftClick();
+    });
+    this.currentIndex = 0;
+    this.imgs[0].style.display = "block";
+  }
 
-//   false;
-// });
+  rightClick() {
+    this.imgs[this.currentIndex].style.display = "none";
+    if (this.currentIndex === this.imgs.length - 1) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex = this.currentIndex + 1;
+    }
+    this.imgs[this.currentIndex].style.display = "block";
+  }
+  leftClick() {
+    this.imgs[this.currentIndex].style.display = "none";
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.imgs.length - 1;
+    } else {
+      this.currentIndex = this.currentIndex - 1;
+    }
+    this.imgs[this.currentIndex].style.display = "block";
+  }
+}
+// console.log("hello")
+let carousel = document.querySelector(".carousel");
+carousel = new Carousel(carousel);
